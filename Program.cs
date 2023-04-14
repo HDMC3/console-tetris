ebb2e9d9378ef3386be6524933413a80e97b2ae9
Hugo Moran <moranh56@gmail.com>
2023-04-14 17:36:56 -0600

    Add window title

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Console.Title = "C#nsole TETRIS";
         Console.Clear();
         var game = new Game();
         game.Start();
